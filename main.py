2151a8448a9f7c41ab10f1bf219554bc97aafd68
Oksana-KV51 <12345-70@mail.ru>
2024-06-30 14:42:11 +0400

    add DZ lesson TG02

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import logging
 import aiohttp
 import asyncio
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from config import TOKEN, WEATHER_API_KEY
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, F, types
 import random
+
+from gtts import gTTS # библиотека для озвучки
+import os
+
+from googletrans import Translator
+translator = Translator()
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -14,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-CITY_NAME = 'Moscow'  # Название города для прогноза погоды
+CITY_NAME = 'Samara'  # Название города для прогноза погоды
 
 # Функция для получения прогноза погоды
 async def get_weather(city_name):
@@ -36,28 +42,98 @@ async def weather(message: Message):
     weather_report = await get_weather(CITY_NAME)
     await message.answer(weather_report)
 
-@dp.message(F. text == "что такое ИИ?")
-async def aitext(message: Message):
-    await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+#отправка видео
+@dp.message(Command('video'))
+async def video(message: Message):
+    await bot.send_chat_action(message.chat.id, 'upload_video')# сообщение о загрузке видео в верху чата
+    video = FSInputFile('video.mp4')
+    await bot.send_video(message.chat.id, video)
 
-@dp.message(Command('image'))
+#отправка аудио
+@dp.message(Command('audio'))
+async def audio(message: Message):
+    audio = FSInputFile('sound1.mp3')
+    await bot.send_audio(message.chat.id, audio)
+
+# озвучивание текста
+@dp.message(Command('training'))
+async def training(message: Message):
+   training_list = [
+       "Тренировка 1: \n1. Скручивания: 3 подхода по 15 повторений \n2. Велосипед: 3 подхода по 20 повторений (каждая сторона) \n3. Планка: 3 подхода по 30 секунд",
+       "Тренировка 2: \n1. Подъемы ног: 3 подхода по 15 повторений \n2. Русский твист: 3 подхода по 20 повторений (каждая сторона) \n3. Планка с поднятой ногой: 3 подхода по 20 секунд (каждая нога)",
+       "Тренировка 3: \n1. Скручивания с поднятыми ногами: 3 подхода по 15 повторений \n2. Горизонтальные ножницы: 3 подхода по 20 повторений \n3. Боковая планка: 3 подхода по 20 секунд (каждая сторона)"
+   ]
+   rand_tr = random.choice(training_list)
+   await message.answer(f"Это ваша мини-тренировка на сегодня {rand_tr}")
+
+   tts = gTTS(text=rand_tr, lang='ru')
+   tts.save("training.ogg") #mp3
+   audio = FSInputFile('training.ogg')#mp3
+   await bot.send_voice(message.chat.id, audio) #audio
+   os.remove("training.ogg")#mp3
+
+# отправка голосового сообщения - sample.ogg
+#@dp.message(Command('voice'))
+#async def voice(message: Message):
+   # voice = FSInputFile("sample.ogg")
+    #await message.answer_voice(voice)
+
+# отправка документа в pdf
+@dp.message(Command('doc'))
+async def doc(message: Message):
+    doc = FSInputFile("TG02.pdf")
+    await bot.send_document(message.chat.id, doc)
+
+@dp.message(Command('image')) #, prefix='%'
 async def image(message: Message):
-    list = ['https://w.forfun.com/fetch/ec/ecff5e0bb65d194e4c17f8fee00d1ebe.jpeg', 'https://img.razrisyika.ru/kart/24/95248-kotiki-kartinki-18.jpg', 'https://koshka.top/uploads/posts/2021-12/1639894342_27-koshka-top-p-kotiki-na-rabochii-29.jpg']
+    list = ['https://w.forfun.com/fetch/ec/ecff5e0bb65d194e4c17f8fee00d1ebe.jpeg',
+            'https://img.razrisyika.ru/kart/24/95248-kotiki-kartinki-18.jpg',
+            'https://koshka.top/uploads/posts/2021-12/1639894342_27-koshka-top-p-kotiki-na-rabochii-29.jpg'
+    ]
     rand_photo = random.choice(list)
     await message.answer_photo(photo=rand_photo, caption='Это супер крутая картинка')
 
-#Прописываем хендлер и варианты ответов:
+
+#Прописываем варианты ответов для полученных фото:
 @dp.message(F.photo)
 async def react_photo(message: Message):
     list = ['Ого, какая фотка!', 'Непонятно, что это такое', 'Не отправляй мне такое больше']
     rand_answ = random.choice(list)
     await message.answer(rand_answ)
+    await bot.download(message.photo[-1], destination=f'img/{message.photo[-1].file_id}.jpg')
+
 @dp.message(Command('help'))
 async def help(message: Message):
     await message.answer("Этот бот умеет выполнять команды: \n /start \n /help \n /photo \n /weather \n /image")
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer('Приветики, я бот!')
+    await message.answer(f'Салют, {message.from_user.first_name}, как дела? Я бот!') #{message.from_user.full_name}
+
+# ответ на любой запрос
+#@dp.message()
+#async def start(message: Message):
+    #await message.answer("Приветики, я бот!")
+
+#@dp.message(F. text == "что такое ИИ?")
+#async def aitext(message: Message):
+    #await message.answer('Искусственный интеллект — это свойство искусственных интеллектуальных систем выполнять творческие функции, которые традиционно считаются прерогативой человека; наука и технология создания интеллектуальных машин, особенно интеллектуальных компьютерных программ')
+@dp.message()
+#async def start(message: Message):
+   # if message.text.lower() == 'test':
+       # await message.answer('Тестируем')
+
+#бот переводчик
+@dp.message(F.text)
+async def handle_text(message: Message):
+    text_to_translate = message.text
+    translated_text = translator.translate(text_to_translate, src='auto', dest='en').text
+    await message.answer(translated_text)
+
+#эхо бот
+# #@dp.message()
+#async def start(message: Message):
+    #await message.send_copy(chat_id=message.chat.id)
+
 
 async def main():
     await dp.start_polling(bot)
